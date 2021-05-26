e5fff45e12fd4f00dc6b3d9ec106aa42a3c752ae
Justin Bird <justin@thewordisbird.dev>
2021-05-26 10:38:20 -0700

    Refactor: App compnent refactor as functional component. Added useAuth custom hook to pass auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavBar from './shared/Navbar/Navbar';
 import InvoiceItems from './views/InvoiceItems/InvoiceItems';
 import InvoiceDetails from './views/InvoiceDetails/InvoiceDetails';
 import Login from './views/Auth/Login';
 import Container from '@material-ui/core/Container';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"; 
-import { withAuth, withAuthProps } from './shared/Firebase/Auth/withAuth';
+import { useAuth } from './shared/Firebase/Auth/useAuth';
 
-type AppProps = withAuthProps;
+const App: React.FC = () => {
+  const { isAuthenticated } = useAuth()
 
-type AppState = {
-  searchQuery: {vendor?: string | null, startDate?: Date | null, endDate?: Date | null};
+  const authenticatedRoutes = (
+    <Switch>
+      <Route path="/details"><InvoiceDetails /></Route>
+      <Route path="/"><InvoiceItems /></Route>
+    </Switch>
+  )
+
+  const nonAuthenticatedRoutes = (
+    <Switch>
+      <Route path="/"><Login /></Route>
+      <Redirect to="/" />
+    </Switch>
+  )
+
+  return (
+    <Router>
+      <NavBar title="Burbank Square"/>
+      <Container maxWidth="md">
+        { 
+          isAuthenticated 
+            ? authenticatedRoutes
+            : nonAuthenticatedRoutes        
+        }
+      </Container>
+    </Router>
+  );
 }
 
-class App extends Component<AppProps, AppState> {
-  state = {
-    searchQuery: {}
-  }
 
-  render() {
-    const { isAuthenticated } = this.props
-    const routes = isAuthenticated
-      ? (
-        <Switch>
-            <Route path="/details"><InvoiceDetails /></Route>
-            <Route path="/"><InvoiceItems /></Route>
-          </Switch>
-        )
-      : (
-        <Switch>
-            <Route path="/"><Login /></Route>
-            <Redirect to="/" />
-          </Switch>
-      )
-
-    return (
-      <Router>
-        <NavBar title="Burbank Square"/>
-        <Container maxWidth="md">
-          { routes }
-        </Container>
-      </Router>
-    );
-  }
-}
-
-export default withAuth(App);
+export default App;
